40576a7e071fa64c0552ffcfd9796ad184178397
JailtonJrPE <junior.recife.pe@gmail.com>
2025-09-05 00:12:17 +0000

    feat(aula-modulo) desenvolvendo uso de modulos com commonjs

---
+++ aula_modulos/igreja.js
@@ -0,0 +1,7 @@
+trindade = require('./trindade/trindade.js')
+
+
+
+trindade.pai("DEUS")
+trindade.filho("JESUS")
+trindade.espirito("SANTO")
---
+++ aula_modulos/trindade/pai.js
@@ -0,0 +1,4 @@
+function pai (nome) {
+    console.log("Oi eu sou o PAI"),nome
+}
+module.exports = pai
